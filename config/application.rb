09a0b11e499f9bbe4a08020eafcf4cea235c5efe
Travis Luckenbaugh <tluckenbaugh@gmail.com>
2025-01-30 19:26:12 -0500

    Public dev URL test

--- config/application.rb
+++ config/application.rb
@@ -23,5 +23,7 @@ module RailsTutorialStore
     #
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
+
+    config.hosts << "green.melvis.site"
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -14,5 +14,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "posts#index"
 
+  root "products#index"
   resources :products
 end
